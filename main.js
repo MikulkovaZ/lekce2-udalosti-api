f53108fc2dfd8e030cbce2c6488f0e7e7bd14dfe
MikulkovaZ <mikulkova.zuzana@seznam.cz>
2021-03-18 13:37:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,70 @@
 // tady je místo pro náš program
+
+
+let text = document.querySelector(".veta");
+
+
+
+function ztucni() {
+  text.classList.toggle("tucne");
+}
+
+function odtucni() {
+    text.classList.toggle("normal");
+  }
+
+
+function cerveny() {
+  text.classList.toggle("red");
+}
+
+
+let aktualniVelikost = 16;
+text.style.fontSize = aktualniVelikost + "px";
+
+function zvetsi() {
+  ++aktualniVelikost;
+  text.style.fontSize = aktualniVelikost + "px";
+}
+
+
+
+function prehraj(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.play();
+}
+
+
+
+function zastav(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.pause();
+}
+
+
+
+function mute() {
+  let zvuk = document.querySelector("#zvuk");
+  zvuk.volume = 0;
+}
+
+
+
+function stredni() {
+  let zvuk = document.querySelector("#zvuk");
+  zvuk.volume = 0.5;
+}
+
+
+
+function zesil() {
+  let zvuk = document.querySelector("#zvuk");
+  zvuk.volume = 1;
+}
+
+
+
+function zacatek() {
+  let zvuk = document.querySelector("#zvuk");
+  zvuk.currentTime = 0;
+}
